33a788e0d9c2e8cc9b709d52283e7186b48855ce
MaxCheng <nokia6102@gmail.com>
2017-08-08 09:42:07 +0800

    Sqlite3   Header 成功，資料庫檔案copy成功

--- Final_MyBook/MyBook/AppDelegate.swift
+++ Final_MyBook/MyBook/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  MyBook
-//
-//  Created by perkin on 2017/8/2.
-//  Copyright © 2017年 perkin. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -13,9 +5,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    //MARK:- SQLite AppDelegate中全區宣告
+    //宣告資料庫連線變數
+    private var db:OpaquePointer? = nil
+    //回傳資料庫連線給其他類別
+    func getDB() -> OpaquePointer?
+    {
+        return db
+    }
+    //MARK:-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    //MARK:- SQLite didFinishLaunchingWithOptions段宣告
+        //取得檔案管理員
+        let fileManager = FileManager.default
+        //取得資料庫來源路徑
+        let sourceDB = Bundle.main.path(forResource: "literature", ofType: "db")
+        //取得資料庫的目的地路徑
+        let destinationDB = NSHomeDirectory() + "/Documents/MyDB.db"
+        print("目的地路徑：\(destinationDB)")
+        //檢查目的地的資料庫是否已經存在
+        if !fileManager.fileExists(atPath: destinationDB)   //如果不存在
+        {
+            if let _ = try? fileManager.copyItem(atPath: sourceDB!, toPath: destinationDB)
+            {
+                print("資料庫檔案已成功複製！")
+            }
+        }
+        //呼叫Sqlite函式庫，開啟目的地資料庫
+        if sqlite3_open(destinationDB, &db) == SQLITE_OK
+        {
+            print("資料庫開啟成功！")
+        }
+        else
+        {
+            print("資料庫開啟失敗！")
+            db = nil
+        }
+    //MARK:-
+        
         return true
     }
 
